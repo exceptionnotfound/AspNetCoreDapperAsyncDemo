2ddba51ca9a2e59c7a98a0924140704abf7d2fb5
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2018-07-09 12:34:42 -0700

    Revert "Corrected indents"
    This reverts commit be54eb7ca2dc17d119929dc0d9ce454c933ab8a1.

--- DapperCoreTest/Startup.cs
+++ DapperCoreTest/Startup.cs
@@ -14,32 +14,32 @@ using Microsoft.Extensions.Options;
 
 namespace DapperCoreTest
 {
-    public class Startup
+public class Startup
+{
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddTransient<IEmployeeRepository, EmployeeRepository>();
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc();
-        }
+        Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+
+        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseMvc();
+    }
+}
 }
